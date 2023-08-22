aa0a4450dae7ce646ab6feb25b1e5418f0a56216
RJ Descartin <jadescartin@gmail.com>
2023-08-23 01:41:44 +0800

    Update button-shortcode.php
    added URL for external links

--- php/button-shortcode/button-shortcode.php
+++ php/button-shortcode/button-shortcode.php
@@ -1,16 +1,34 @@
 <?php 
-    function button_shortcode($atts, $content = null) {
+
+// Use for internal links
+function button_shortcode($atts, $content = null) {
     extract(shortcode_atts(array(
         "href" => 'http://',
         "style" => '',
     ), $atts));
-
+    
     $classes = 'button';
     if ($style) {
         $classes .= ' button-' . sanitize_html_class($style);
     }
-
+    
     return '<a href="' . esc_url($href) . '" class="' . esc_attr($classes) . '">' . do_shortcode($content) . '</a>';
-    }
-    add_shortcode("button", "button_shortcode");
-?>
+}
+add_shortcode("button", "button_shortcode");
+
+
+// Use for external links
+function url_shortcode($atts, $content = null) {
+	extract(shortcode_atts(array(
+		"href" => 'http://',
+		"style" => '',
+	), $atts));
+
+	$classes = 'button';
+	if ($style) {
+		$classes .= ' button-' . sanitize_html_class($style);
+	}
+
+	return '<a href="' . esc_url($href) . '" class="' . esc_attr($classes) . '" target="_blank" rel="nofollow">' . do_shortcode($content) . '</a>';
+}
+add_shortcode("url", "url_shortcode");
