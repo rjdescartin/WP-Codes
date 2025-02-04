5c8e892b2e764eb17f41e61c0fd8475dd54b855d
RJ Descartin <jadescartin@gmail.com>
2025-02-05 01:10:18 +0800

    Update button-shortcode.php

--- functions/button-shortcode/button-shortcode.php
+++ functions/button-shortcode/button-shortcode.php
@@ -1,6 +1,18 @@
 <?php 
 
-// Use for internal links
+/**
+ * Button Shortcode (Internal Links)
+ * 
+ * Creates a styled button for internal links using the [button] shortcode.
+ * The button's URL and optional style can be customized.
+ * 
+ * Usage:
+ * [button href="https://example.com" style="primary"]Click Me[/button]
+ * 
+ * @param array  $atts    Shortcode attributes (expects 'href' and 'style').
+ * @param string $content Content enclosed within the shortcode (button text).
+ * @return string HTML output for the styled button.
+ */
 function button_shortcode($atts, $content = null) {
     extract(shortcode_atts(array(
         "href" => 'http://',
@@ -17,7 +29,19 @@ function button_shortcode($atts, $content = null) {
 add_shortcode("button", "button_shortcode");
 
 
-// Use for external links
+/**
+ * URL Shortcode (External Links)
+ * 
+ * Creates a styled button for external links using the [url] shortcode.
+ * Adds `target="_blank"` to open links in a new tab and `rel="nofollow"` for SEO best practices.
+ * 
+ * Usage:
+ * [url href="https://external.com" style="secondary"]Visit Site[/url]
+ * 
+ * @param array  $atts    Shortcode attributes (expects 'href' and 'style').
+ * @param string $content Content enclosed within the shortcode (button text).
+ * @return string HTML output for the external link button.
+ */
 function url_shortcode($atts, $content = null) {
 	extract(shortcode_atts(array(
 		"href" => 'http://',
