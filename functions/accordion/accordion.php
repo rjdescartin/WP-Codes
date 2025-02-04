981a504f2932e2e0b73dde0534d972d28bff03ab
RJ Descartin <jadescartin@gmail.com>
2025-02-05 01:09:00 +0800

    Update accordion.php

--- functions/accordion/accordion.php
+++ functions/accordion/accordion.php
@@ -1,5 +1,19 @@
 <?php
 
+/**
+ * Custom Accordion Shortcode
+ * 
+ * This function registers a WordPress shortcode [accordion] that creates a collapsible 
+ * accordion section with a button and a content panel. The label of the button can be 
+ * customized using the 'label' attribute.
+ * 
+ * Usage:
+ * [accordion label="Click Me"]Your content here[/accordion]
+ * 
+ * @param array  $atts    Shortcode attributes (expects 'label').
+ * @param string $content Content enclosed within the shortcode.
+ * @return string HTML output for the accordion component.
+ */
 function custom_accordion_shortcode($atts, $content = null) {
     // Extract the label attribute
     $atts = shortcode_atts(array(
