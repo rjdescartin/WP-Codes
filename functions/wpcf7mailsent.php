7af2e725be98c0baac4123cce827611764866c00
RJ Descartin <jadescartin@gmail.com>
2025-02-11 19:28:17 +0800

    Create wpcf7mailsent.php

---
+++ functions/wpcf7mailsent.php
@@ -0,0 +1,16 @@
+<?php 
+// Add this script to footer.php to handle redirections after successful form submissions 
+// using Contact Form 7's 'wpcf7mailsent' event.
+
+<script>
+  document.addEventListener('wpcf7mailsent', function(event) {
+    
+    // Redirect after form submission
+    if (event.detail.contactFormId === 'CF7_FORM_ID') {
+      location.href = '<?php echo get_permalink(PAGE_ID); ?>';
+    }
+
+  }, false);
+</script>
+
+?>
